688149d2969b7e1cd1b7a24cb054ca927e794d44
ilj13253 <butackovilj25@gmail.com>
2024-03-20 21:16:45 +0800

    DZ

---
+++ HHHTTPS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHHTTPS
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            for (int i = 0; i < 10000000; i++)
+            {
+                string url = $"https://www.google.com/search?q={Guid.NewGuid()}"; 
+                var http = new HttpClient();
+                var response = await http.GetAsync(url);
+                Console.WriteLine($"Запрос: {i + 1}: {(int)response.StatusCode}");
+                tasks.Add(response.Content.ReadAsStringAsync());
+            }
+            await Task.WhenAll(tasks);
+            Console.WriteLine("Все запросы завершены.");
+            Console.ReadKey();
+        }
+    }
+}
+//Написать свой тестировщик где вы указывете путь и количество запросов и происходит атака на этот сайт
